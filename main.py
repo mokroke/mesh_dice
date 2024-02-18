682f2e4f70f2e3433fb8d9520b1eb65823e96011
mokroke <pararerusupekku@icloud.com>
2024-02-18 17:38:55 +0900

    メッシュの判定を追加

--- main.py
+++ main.py
@@ -1,14 +1,24 @@
 import streamlit as st
 import worldmesh as wm
+from shapely.geometry import Point
+from shapely.geometry.polygon import Polygon
 import folium
 
 
+def parse_coordinates(coordinates):
+    # 括弧を取り除く
+    coordinates = coordinates.replace("(", "").replace(")", "")
+    # カンマで分割してlatとlngに格納
+    lat, lng = coordinates.split(",")
+    return float(lat), float(lng)
+
+
 def main():
-    st.title("Simple Square Calculator")
+    st.title("メッシュに行ってみよう！")
 
     meshcode3 = 2052345020
 
-    st.write(f"MESHCODE : {meshcode3}")
+    st.write(f"## MESHCODE3 : {meshcode3}")
 
     ##地図パート
 
@@ -45,6 +55,41 @@ def main():
     # Streamlitで地図を表示
     # st.folium_static(m)
     st.components.v1.html(folium.Figure().add_child(m).render(), height=500)
+
+    ##入力情報
+    coordinates_input = st.text_input(
+        "Enter coordinates (e.g., 35.10207863776157, 134.00614494784006):"
+    )
+
+    # 入力が空でない場合のみ処理
+    if coordinates_input:
+        # 座標を解析してlatとlngに格納
+        lat, lng = parse_coordinates(coordinates_input)
+
+        # 結果を表示
+        # st.write(f"Latitude: {lat},Longitude: {lng}")
+
+        latlon = wm.meshcode_to_latlong_grid(meshcode3)
+
+        # ポリゴンの頂点座標を定義
+        polygon_coordinates = [
+            (latlon["lat0"], latlon["long0"]),
+            (latlon["lat1"], latlon["long0"]),
+            (latlon["lat0"], latlon["long1"]),
+            (latlon["lat1"], latlon["long1"]),
+        ]
+
+        # ポリゴンを作成
+        polygon = Polygon(polygon_coordinates)
+
+        # テストする緯度経度座標を指定
+        test_point = Point(lat, lng)  # この座標がポリゴン内に含まれているかを確認
+
+        # ポリゴン内に含まれているかどうかを判定
+        if polygon.contains(test_point):
+            st.write(f"### {meshcode3}です！")
+        else:
+            st.write(f"### {meshcode3}ではありません！")
 
 
 if __name__ == "__main__":
